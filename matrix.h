179575f0c38de67b44444bc53e8ebdd8d3dcf85c
Seiji Leroux <45913147+imroux@users.noreply.github.com>
2026-03-23 22:46:49 +0300

    matrix.h cleanup

--- matrix.h
+++ matrix.h
@@ -1,15 +1,16 @@
-#ifndef MATRIX_H
+#ifndef MATRIX_H // Prevents duplicate includes
 #define MATRIX_H
 
 #include <string>
 #include <vector>
 
+using namespace std;
+
 // Function declarations (prototypes)
-bool readMatrix(const std::string &filename,
-                std::vector<std::vector<int>> &matrix, int &rows, int &cols);
-void printMatrix(const std::string &name,
-                 const std::vector<std::vector<int>> &matrix, int rows,
-                 int cols);
+bool readMatrix(const string &filename, vector<vector<int>> &matrix, int &rows,
+                int &cols);
+void printMatrix(const string &name, const vector<vector<int>> &matrix,
+                 int rows, int cols);
 bool validateInputDimensions(int rowsA, int colsA, int rowsB, int colsB);
 
 #endif // MATRIX_H
